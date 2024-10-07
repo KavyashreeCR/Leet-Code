70b12acacf758a62cc24e3efcee7de437c27f902
Kavyashree <162327118+KavyashreeCR@users.noreply.github.com>
2024-10-07 13:43:53 +0530

    Time: 4 ms (85.19%), Space: 8.5 MB (76.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     bool isPalindrome(int x) 
     {
-        // If x is negative, it cannot be a palindrome
-        if (x < 0) return false;
-        
-        // Save the original value of x
-        int original = x;
-        int rev = 0;
-        
-        // Reverse the integer
-        while (x != 0) {
-            int n = x % 10;
-            if (rev > (INT_MAX / 10) || (rev == INT_MAX / 10 && n > 7)) {
-                return false;  // It will overflow, so it's not a palindrome
-            }
-            rev =(rev * 10) + n;
-            x = x / 10;
+       if(x<0){return 0;}
+        int rev=0;
+        int c=x;
+        int lastdigit;
+        while(x!=0)
+        {
+            lastdigit=x%10;
+           
+             if(rev>INT_MAX/10||(rev==INT_MAX/10 && lastdigit>7)){
+                return 0;}
+             rev=rev*10+lastdigit;
+                 
+              x=x/10;
+        };
+       
+            return rev==c;
         }
-        
-        // Compare the reversed number with the original
-        return rev == original;
-    }
 };
